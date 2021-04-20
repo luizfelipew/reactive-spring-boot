53fe37291e1b33c09394449bc20c2b0fe96be8ce
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-04-19 22:33:59 -0300

    add: update an Item Endpoint

--- src/main/java/com/learn/dev/reactivespring/controller/v1/ItemController.java
+++ src/main/java/com/learn/dev/reactivespring/controller/v1/ItemController.java
@@ -41,4 +41,17 @@ public class ItemController {
     public Mono<Void> deleteItem(@PathVariable String id) {
         return itemReactiveRepository.deleteById(id);
     }
+
+    @PutMapping(ITEM_END_POINT_V1 + "/{id}")
+    public Mono<ResponseEntity<Item>> updateItem(@PathVariable String id,
+                                                 @RequestBody Item item) {
+        return itemReactiveRepository.findById(id)
+                .flatMap(currentItem -> {
+                    currentItem.setPrice(item.getPrice());
+                    currentItem.setDescription(item.getDescription());
+                    return itemReactiveRepository.save(currentItem);
+                })
+                .map(updatedItem -> new ResponseEntity<>(updatedItem, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
