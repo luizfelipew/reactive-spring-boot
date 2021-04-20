68d997e3da140a184ec4a6754a8d16cf1f0371d5
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-04-19 22:16:31 -0300

    add: delete an Item Endpoint

--- src/test/java/com/learn/dev/reactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/learn/dev/reactivespring/controller/v1/ItemControllerTest.java
@@ -137,4 +137,17 @@ class ItemControllerTest {
                 .jsonPath("$.description").isEqualTo("Iphone X")
                 .jsonPath("$.price").isEqualTo(999.99);
     }
+
+    @Test
+    void deleteItem() {
+        webTestClient
+                .delete()
+                .uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "ABC")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Void.class);
+
+
+    }
 }
