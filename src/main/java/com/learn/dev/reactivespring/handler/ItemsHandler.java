66b8fb514d0a11687fee9a7a3e990c2b07ba1350
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-04-23 19:43:26 -0300

    add: get All Items - Functional Endpoint

--- src/main/java/com/learn/dev/reactivespring/handler/ItemsHandler.java
+++ src/main/java/com/learn/dev/reactivespring/handler/ItemsHandler.java
@@ -1,8 +1,13 @@
 package com.learn.dev.reactivespring.handler;
 
+import com.learn.dev.reactivespring.document.Item;
 import com.learn.dev.reactivespring.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @Component
 public class ItemsHandler {
@@ -10,5 +15,9 @@ public class ItemsHandler {
     @Autowired
     ItemReactiveRepository itemReactiveRepository;
 
-
+    public Mono<ServerResponse> getAllItems(ServerRequest serverRequest) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemReactiveRepository.findAll(), Item.class);
+    }
 }
