b0383aba5a12233e23b41ca58913cf487fb4d17d
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-04-26 18:58:06 -0300

    add: Get All Items - JUNIT Test using WebTestClient

---
+++ src/test/java/com/learn/dev/reactivespring/handler/ItemHandlerTest.java
@@ -0,0 +1,65 @@
+package com.learn.dev.reactivespring.handler;
+
+import com.learn.dev.reactivespring.constants.ItemConstants;
+import com.learn.dev.reactivespring.document.Item;
+import com.learn.dev.reactivespring.repository.ItemReactiveRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemHandlerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+        return asList(new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 329.99),
+                new Item(null, "Apple Watch", 349.99),
+                new Item("ABC", "Beats HeadPhones", 149.99));
+    }
+
+    @BeforeEach
+    void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> {
+                    System.out.println("Inserted item is : " + item);
+                })
+                .blockLast();
+    }
+
+    @Test
+    void getAllItems() {
+        webTestClient
+                .get()
+                .uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4);
+    }
+}
