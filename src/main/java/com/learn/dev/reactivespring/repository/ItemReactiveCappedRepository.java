5d0c5348c60e1881c632d9983caf924d2fa92486
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-05-19 23:05:36 -0300

    add: Build the Reactive ItemCapped Mongo Repository

---
+++ src/main/java/com/learn/dev/reactivespring/repository/ItemReactiveCappedRepository.java
@@ -0,0 +1,13 @@
+package com.learn.dev.reactivespring.repository;
+
+import com.learn.dev.reactivespring.document.ItemCapped;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ItemReactiveCappedRepository extends ReactiveMongoRepository<ItemCapped, String> {
+
+    @Tailable
+    Flux<ItemCapped> findItemsBy();
+
+}
