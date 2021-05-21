6955bddfb6a441c593dbaf70695d4307d386b57c
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-05-21 08:06:19 -0300

    add: nitialize the data using CommandLineRunner

--- src/main/java/com/learn/dev/reactivespring/initialize/ItemDataInitializer.java
+++ src/main/java/com/learn/dev/reactivespring/initialize/ItemDataInitializer.java
@@ -2,20 +2,23 @@ package com.learn.dev.reactivespring.initialize;
 
 import com.learn.dev.reactivespring.document.Item;
 import com.learn.dev.reactivespring.document.ItemCapped;
+import com.learn.dev.reactivespring.repository.ItemReactiveCappedRepository;
 import com.learn.dev.reactivespring.repository.ItemReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.CollectionOptions;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
+@Slf4j
 @Component
 @Profile("!test")
 public class ItemDataInitializer implements CommandLineRunner {
@@ -24,12 +27,16 @@ public class ItemDataInitializer implements CommandLineRunner {
     ItemReactiveRepository itemReactiveRepository;
 
     @Autowired
+    ItemReactiveCappedRepository itemReactiveCappedRepository;
+
+    @Autowired
     ReactiveMongoOperations mongoOperations;
 
     @Override
     public void run(String... args) throws Exception {
         initialDataSetup();
         createCappedCollection();
+        dataSetUpforCappedCollection();
     }
 
     private void createCappedCollection() {
@@ -42,6 +49,17 @@ public class ItemDataInitializer implements CommandLineRunner {
                 new Item(null, "LG TV", 329.99),
                 new Item(null, "Apple Watch", 349.99),
                 new Item("ABC", "Beats HeadPhones", 149.99));
+    }
+
+    public void dataSetUpforCappedCollection() {
+        Flux<ItemCapped> itemCappedFlux = Flux.interval(Duration.ofSeconds(1))
+                .map(i -> new ItemCapped(null, "Random Item " + i, (100.00 + i)));
+
+        itemReactiveCappedRepository
+                .insert(itemCappedFlux)
+                .subscribe(itemCapped -> {
+                    log.info("Inserted Item is " + itemCapped);
+                });
     }
 
     private void initialDataSetup() {
